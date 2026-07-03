7dde3322d5e0bd4c4293ef9e38f5cb65226537ee
Rahena-m <shaikrahena01@gmail.com>
2026-07-03 12:42:47 +0530

    Initial project setup

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Welcome message
+console.log("Welcome to VIT-AP Tech Fest 2026!");
+
+// Register button interaction
+const registerBtn = document.querySelector(".btn");
+
+registerBtn.addEventListener("click", () => {
+    alert("Redirecting to Registration Page...");
+});
